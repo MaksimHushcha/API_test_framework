b8b6e117725b5895dbe5f2f83320e127f93a1c4d
Maksim Hushcha <maksim.hushcha@innowise.com>
2026-07-04 19:43:05 +0200

    added fixture for failed by design tests

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -22,13 +22,14 @@ def test_get_post_by_id(posts_endpoints, generate_random_id):
 
 def test_get_comments_to_the_post(comments_endpoints, generate_random_id):
     (
-        comments_endpoints.get_comments_to_the_post(generate_random_id)
+        posts_endpoints.get_comments_to_the_post(generate_random_id)
         .assert_status(200)
         .assert_schema("comments_schema.json")
         .assert_returned_key_value(expected_key="postId", expected_value=generate_random_id)
         .assert_sorted_by_id()
     )
 
+@pytest.mark.xfail("Fake API, designed to fail")
 def test_create_a_new_post(posts_endpoints, generate_a_post_payload, delete_post, get_post):
     response = (
         posts_endpoints.create_a_post(generate_a_post_payload)
@@ -44,7 +45,7 @@ def test_create_a_new_post(posts_endpoints, generate_a_post_payload, delete_post
     assert actual_post_response.json_data == response.json_data, \
         f"Expected to get a new post with requested content {response.json_data}, but got {actual_post_response.json_data}"
 
-
+@pytest.mark.xfail("Fake API, designed to fail")
 def test_replace_a_post(posts_endpoints, create_and_teardown_the_post, generate_a_post_payload, get_post):
     # we can swap userID while modifying the post
     response = (
